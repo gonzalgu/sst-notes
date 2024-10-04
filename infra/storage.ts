bdcf67e1dc4d2dffd51c856ef91b6dd46804534b
gonzalgu <gadol.gonzalez@gmail.com>
2024-10-04 15:26:09 -0400

    Adding storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,13 @@
 export const bucket = new sst.aws.Bucket("Uploads");
+
+//create a dynamodb table
+export const table = new sst.aws.Dynamo("Notes", {
+    fields:{
+        userId: "string",
+        noteId: "string",
+    },
+    primaryIndex: {
+        hashKey: "userId",
+        rangeKey: "noteId"
+    },
+});
